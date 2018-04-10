e6e7bb620b56ec80e76d8e8fa62a51ade9a36ecb
franck-benault <franck-benault@orange.fr>
2018-04-10 22:37:24 +0200

    complete version java7

---
+++ src/main/java/org/shapleyvalue/extension/taxi/TaxiCalculation.java
@@ -0,0 +1,86 @@
+package org.shapleyvalue.extension.taxi;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.shapleyvalue.CharacteristicFunction;
+import org.shapleyvalue.CharacteristicFunction.CharacteristicFunctionBuilder;
+import org.shapleyvalue.ShapleyValue;
+import org.shapleyvalue.util.SubSets;
+
+public class TaxiCalculation {
+	
+	private CharacteristicFunction cfunction;
+	private ShapleyValue shapleyValue;
+	private Map<Integer, String> range;
+	
+	private TaxiCalculation(TaxiCalculationBuilder builder) {
+
+		Set<Set<Integer>> sets = SubSets.getAllSubSetsNonEmpty(builder.getNbPlayers());
+
+		CharacteristicFunctionBuilder cfunctionBuilder = 
+				new CharacteristicFunction.CharacteristicFunctionBuilder(builder.getNbPlayers());
+		
+		for(Set<Integer> set : sets) {
+			double max =0;
+			for(Integer i : set) {
+				double val = builder.getV().get(i);
+				if(val>max) max=val;
+			}
+			cfunctionBuilder.addCoalition(max, set.toArray(new Integer[set.size()]));
+		}
+		range = builder.getRange();
+		cfunction = cfunctionBuilder.build();
+	}
+	
+	
+	public static class TaxiCalculationBuilder {
+		private int nbPlayers;
+		private Map<Integer, Double> v;
+		private Map<Integer, String> range;
+
+		public TaxiCalculationBuilder() {
+			nbPlayers = 0;
+			v = new HashMap<>();
+			range = new HashMap<>();
+		}
+
+		public TaxiCalculationBuilder addUser(double value, String userName) {
+			nbPlayers ++;
+			v.put(nbPlayers, value);
+			range.put(nbPlayers, userName);
+			return this;
+		}
+
+		public TaxiCalculation build() {
+			return new TaxiCalculation(this);
+		}
+
+		public int getNbPlayers() {
+			return nbPlayers;
+		}
+		
+		public  Map<Integer, Double> getV() {
+			return v;
+		}
+		
+		public  Map<Integer, String> getRange() {
+			return range;
+		}
+
+	}
+
+
+	public Map<String, Double> calculate() {
+		shapleyValue = new ShapleyValue(cfunction);
+		Map<Integer, Double> tempRes = shapleyValue.calculate();
+		Map<String, Double> res = new HashMap<>();
+		for(Integer i : tempRes.keySet()) {
+			res.put(range.get(i), tempRes.get(i));
+		}
+		return res;
+		
+	}
+
+}
---
+++ src/test/java/org/shapleyvalue/extension/taxi/TaxiCalculationTest.java
@@ -0,0 +1,65 @@
+package org.shapleyvalue.extension.taxi;
+
+import static org.junit.Assert.*;
+
+import java.util.Map;
+
+import org.junit.Test;
+
+public class TaxiCalculationTest {
+
+	@Test
+	public void testCalculateOneParticipant() {
+		
+		TaxiCalculation taxiCalculation = 
+				new TaxiCalculation.TaxiCalculationBuilder()
+				.addUser(1.0, "A").build();
+	
+		
+		Map<String,Double> output = taxiCalculation.calculate();
+		double phiA = output.get("A");
+		
+		assertEquals(phiA, 1.0, 0.01);
+	}
+	
+
+	@Test
+	public void testCalculateTwoParticipants() {
+		
+		TaxiCalculation taxiCalculation = 
+				new TaxiCalculation.TaxiCalculationBuilder()
+				.addUser(1.0, "A")
+				.addUser(1.0, "B")
+				.build();
+
+		
+		Map<String,Double> output = taxiCalculation.calculate();
+		double phiA = output.get("A");
+		double phiB = output.get("B");
+		
+		
+		assertEquals(phiA, 0.5, 0.01);
+		assertEquals(phiA, phiB, 0.01);
+		
+	}
+	
+	@Test
+	public void testCalculateThreeParticipants() {		
+		TaxiCalculation taxiCalculation = 
+				new TaxiCalculation.TaxiCalculationBuilder()
+				.addUser(6.0, "A")
+				.addUser(12.0, "B")
+				.addUser(42.0, "C")
+				.build();
+
+		Map<String,Double> output = taxiCalculation.calculate();
+		double phiA = output.get("A");
+		double phiB = output.get("B");
+		double phiC = output.get("C");
+				
+		assertEquals(phiA, 2.0, 0.01);
+		assertEquals(phiB, 5.0, 0.01);
+		assertEquals(phiC, 35.0, 0.01);	
+	}
+
+}
