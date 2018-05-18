28d101a9f657e3176f4c772f85ea079f4d517dfe
franck-benault <franck-benault@orange.fr>
2018-05-18 23:03:44 +0200

    [PARALLELISM] first example with paraliament

---
+++ src/main/java/org/shapleyvalue/application/impl/parliament/multithread/ParliamentCalculationTask.java
@@ -0,0 +1,54 @@
+package org.shapleyvalue.application.impl.parliament.multithread;
+
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+
+import org.shapleyvalue.core.CharacteristicFunction;
+import org.shapleyvalue.core.ShapleyValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * Application of the Shapley value for a parliament
+ * with parallelism
+ * Task
+ * 
+ * 
+ * @author Franck Benault
+ * 
+ * @version	0.0.2
+ * @since 0.0.2
+ *
+ */
+public class ParliamentCalculationTask implements Callable<Map<Integer, Double>> {
+	
+	private final Logger logger = LoggerFactory.getLogger(ParliamentCalculationTask.class);
+
+	
+	private ShapleyValue shapleyValue;
+	private long nbCoalitions;
+	private int threadNumber;
+
+	
+	public ParliamentCalculationTask(CharacteristicFunction cfunction, long nbCoalitions, int threadNumber) {
+		this.nbCoalitions = nbCoalitions;
+		this.threadNumber = threadNumber;
+		shapleyValue = new ShapleyValue(cfunction);
+	}
+	
+
+
+	@Override
+	public Map<Integer, Double> call() throws Exception {
+		logger.info("thread {} started", threadNumber);
+		
+		shapleyValue.calculate(nbCoalitions, true);
+		logger.info("thread {} end", threadNumber);
+		
+		return shapleyValue.getResult();
+	}
+	
+
+}
--- src/test/java/microbenchmark/ParliamentCalculationBenchmarkTest.java
+++ src/test/java/microbenchmark/ParliamentCalculationBenchmarkTest.java
@@ -1,14 +1,16 @@
 package microbenchmark;
 
 
+import static org.junit.Assert.assertTrue;
+
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.shapleyvalue.application.facade.CoalitionStrategy;
 import org.shapleyvalue.application.facade.ShapleyApplicationException;
 import org.shapleyvalue.application.impl.parliament.ParliamentCalculation;
+import org.shapleyvalue.application.impl.parliament.multithread.ParliamentCalculationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,31 +20,15 @@ public class ParliamentCalculationBenchmarkTest {
 
 	private final Logger logger = LoggerFactory.getLogger(ParliamentCalculationBenchmarkTest.class);
 
-
-
-	@Test
-	@Ignore
-	public void testExampleBelgium() throws ShapleyApplicationException {
-
-		Stopwatch stopwatch = Stopwatch.createStarted();
-		ParliamentCalculation parlementCalculation = new ParliamentCalculation.ParliamentCalculationBuilder()
-				.addParty("NVA", 31).addParty("PS", 23).addParty("MR", 20).addParty("CD&V", 18).addParty("openVLD", 14)
-				.addParty("PSA", 13).addParty("EcoloGroen", 12).addParty("CDH", 9).addParty("VB", 3).addParty("Defi", 2)
-				.addParty("PTB", 2).addParty("VW", 2).addParty("PP", 1).build();
-
-		Map<String, Double> output = parlementCalculation.calculate(1_000_000, CoalitionStrategy.RANDOM);
 	
-		stopwatch.stop();
-		long millis = stopwatch.elapsed(TimeUnit.SECONDS);
-		logger.info("that took: {}", millis);
-		
+	private void showResult(Map<String, Double> output) {
 		double phiNVA = output.get("NVA");
 		logger.info("phiNVA= {}", String.format("%.3f", phiNVA));
 
 		double phiPS = output.get("PS");
 		logger.info("phiPS= {}", String.format("%.3f", phiPS));
 
-		double phiMR = output.get("MR");
+		/*double phiMR = output.get("MR");
 		logger.info("phiMR= {}", String.format("%.3f", phiMR));
 
 		double phiCDV = output.get("CD&V");
@@ -73,8 +59,64 @@ public class ParliamentCalculationBenchmarkTest {
 		logger.info("phiVW= {}", String.format("%.3f", phiVW));
 
 		double phiPP = output.get("PP");
-		logger.info("phiPP= {}", String.format("%.3f", phiPP));
+		logger.info("phiPP= {}", String.format("%.3f", phiPP));*/
+	}
+
+
+	@Test
+	public void testExampleBelgium() throws ShapleyApplicationException {
+		
+		long sampleSize = 1_000_000;
+
+		Stopwatch stopwatch = Stopwatch.createStarted();
+		ParliamentCalculation parlementCalculation1 = new ParliamentCalculation.ParliamentCalculationBuilder()
+				.addParty("NVA", 31).addParty("PS", 23).addParty("MR", 20).addParty("CD&V", 18).addParty("openVLD", 14)
+				.addParty("PSA", 13).addParty("EcoloGroen", 12).addParty("CDH", 9).addParty("VB", 3).addParty("Defi", 2)
+				.addParty("PTB", 2).addParty("VW", 2).addParty("PP", 1).build();
+
+		Map<String, Double> output = parlementCalculation1.calculate(sampleSize, CoalitionStrategy.RANDOM);
+	
+		stopwatch.stop();
+		long duration1 = stopwatch.elapsed(TimeUnit.SECONDS);
+		logger.info("that took: {}", duration1);
+		
+		showResult(output);
+		
+		
+		stopwatch.reset();
+		stopwatch.start();
+		ParliamentCalculationService parlementCalculation2 = new ParliamentCalculationService.ParliamentCalculationBuilder()
+				.addParty("NVA", 31).addParty("PS", 23).addParty("MR", 20).addParty("CD&V", 18).addParty("openVLD", 14)
+				.addParty("PSA", 13).addParty("EcoloGroen", 12).addParty("CDH", 9).addParty("VB", 3).addParty("Defi", 2)
+				.addParty("PTB", 2).addParty("VW", 2).addParty("PP", 1).build();
+
+		output = parlementCalculation2.calculate(sampleSize, 1);
+	
+		stopwatch.stop();
+		long duration2 = stopwatch.elapsed(TimeUnit.SECONDS);
+		logger.info("that took: {}", duration2);
+		
+		showResult(output);
+		
+		stopwatch.reset();
+		stopwatch.start();
+		ParliamentCalculationService parlementCalculation3 = new ParliamentCalculationService.ParliamentCalculationBuilder()
+				.addParty("NVA", 31).addParty("PS", 23).addParty("MR", 20).addParty("CD&V", 18).addParty("openVLD", 14)
+				.addParty("PSA", 13).addParty("EcoloGroen", 12).addParty("CDH", 9).addParty("VB", 3).addParty("Defi", 2)
+				.addParty("PTB", 2).addParty("VW", 2).addParty("PP", 1).build();
+
+		output = parlementCalculation3.calculate(sampleSize, 5);
+	
+		stopwatch.stop();
+		long duration3 = stopwatch.elapsed(TimeUnit.SECONDS);
+		logger.info("that took: {}", duration3);
+		
+		showResult(output);
+		
+		assertTrue(duration3< duration2);
+		assertTrue(duration3< duration1);
 
 	}
+
 
 }
