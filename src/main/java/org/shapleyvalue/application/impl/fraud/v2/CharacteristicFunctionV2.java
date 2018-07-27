07f4786d3426cc8b53844d461e4bcc13fba687b9
franck-benault <franck-benault@orange.fr>
2018-07-27 23:17:18 +0200

    [CLEAN] clean import

--- src/main/java/org/shapleyvalue/application/impl/fraud/v2/CharacteristicFunctionV2.java
+++ src/main/java/org/shapleyvalue/application/impl/fraud/v2/CharacteristicFunctionV2.java
@@ -2,7 +2,6 @@ package org.shapleyvalue.application.impl.fraud.v2;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
